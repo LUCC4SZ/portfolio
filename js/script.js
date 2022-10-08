e58f07f41ea3a1adaccd16df549adc0a5ac9777a
LuccaSZ <luccazamora@gmail.com>
2022-10-08 16:36:35 -0300

    Subiendo por primera vez mi portfolio personal

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var img=document.getElementById("foto")
+function cambiar (){
+    img.src="img/IMG_20220707_192459784.jpg"
+    //img.style.transition="5s"
+}
+function volver (){
+    img.src="img/IMG_20220707_192511316.jpg"
+    //img.style.transition="5s"
+}
